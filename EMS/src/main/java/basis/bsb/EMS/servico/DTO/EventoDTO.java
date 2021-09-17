c2e12f04bcfc731d1b43b4894f0221f3c425b569
estevao97i <pedro.cordova2002@gmail.com>
2021-09-17 13:58:49 -0300

    map struct pronto e Mapper Entity

--- EMS/src/main/java/basis/bsb/EMS/servico/DTO/EventoDTO.java
+++ EMS/src/main/java/basis/bsb/EMS/servico/DTO/EventoDTO.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.sql.Select;
 
 import java.time.LocalDateTime;
 
---
+++ EMS/src/main/java/basis/bsb/EMS/servico/Mapper/UsuarioMapper.java
@@ -0,0 +1,9 @@
+package basis.bsb.EMS.servico.Mapper;
+
+import basis.bsb.EMS.dominio.Usuario;
+import basis.bsb.EMS.servico.DTO.UsuarioDTO;
+
+
+public interface UsuarioMapper extends EntityMapper<UsuarioDTO, Usuario> {
+
+}
