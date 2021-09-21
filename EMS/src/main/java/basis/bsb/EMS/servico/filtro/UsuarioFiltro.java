30d515e3e790403e7a54880c530f7707d86001fe
cardoso21 <wagnerjcardoso21@gmail.com>
2021-09-20 21:56:11 -0300

    atualizaçao dos filtors de pesquisa

--- EMS/src/main/java/basis/bsb/EMS/servico/filtro/UsuarioFiltro.java
+++ EMS/src/main/java/basis/bsb/EMS/servico/filtro/UsuarioFiltro.java
@@ -1,5 +1,6 @@
 package basis.bsb.EMS.servico.filtro;
 
+import basis.bsb.EMS.dominio.Cargo_;
 import basis.bsb.EMS.dominio.Usuario;
 import basis.bsb.EMS.dominio.Usuario_;
 import lombok.Getter;
@@ -13,24 +14,43 @@ import javax.persistence.criteria.Root;
 import java.util.ArrayList;
 import java.util.List;
 
+
+
 @Getter
 @Setter
 public class UsuarioFiltro implements EntityFiltro<Usuario>{
 
     private Long id;
-    private String name;
+    private String nome;
     private String cargo;
+    private String cpf;
 
     @Override
     public Specification<Usuario> filter(){
-        return ((root, query, criteriaBuilder) -> criteriaBuilder.and(getPredicates(root,query,criteriaBuilder)
-                .toArray(new Predicate[0])));
+        return (root, query, criteriaBuilder) -> criteriaBuilder.and(getPredicates(root,query,criteriaBuilder)
+                .toArray(new Predicate[0]));
     }
 
     private List<Predicate> getPredicates(Root<Usuario> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
 
         List<Predicate> predicates = new ArrayList<>();
         criteriaQuery.orderBy(criteriaBuilder.desc(root.get(Usuario_.id)));
+
+        if(nome != null){
+            predicates.add(criteriaBuilder.like(root.get(Usuario_.nome),"%" + nome +"%"));
+        }
+
+        if (cpf != null){
+            predicates.add(criteriaBuilder.like(root.get(Usuario_.CPF),"%" + cpf + "%"));
+        }
+
+        if(cargo != null){
+            predicates.add(criteriaBuilder.like(root.join("cargo").get(String.valueOf(Cargo_.descricao)),"%" + cargo + "%"));
+        }
+
+        if(id != null){
+            predicates.add(criteriaBuilder.equal(root.get(Usuario_.id),id));
+        }
 
         return predicates;
     }
