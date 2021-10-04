04cda5466d107fbc9e624c768aab10d9a191c5ab
estevao97i <pedro.cordova2002@gmail.com>
2021-10-04 14:19:42 -0300

    alteracao create usuario

--- frontend/src/app/components/usuario/usuario-create/usuario-create.component.ts
+++ frontend/src/app/components/usuario/usuario-create/usuario-create.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Router} from '@angular/router';
-import { FormGroup } from '@angular/forms';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import {UsuarioService} from '../../../Service/usuario.service';
+import { SelectItem } from 'primeng';
+import { CargoService } from 'src/app/Service/cargo.service';
 
 
 
@@ -13,31 +14,52 @@ import {UsuarioService} from '../../../Service/usuario.service';
 })
 export class UsuarioCreateComponent implements OnInit {
 
-    // public cargos: SelectedItem[] = [];
+    @Input() criaUsuario: boolean = true;
+
+    public cargos: SelectItem[] = [];
 
     public form: FormGroup;
+    public formBuilder: FormBuilder = new FormBuilder;
+
 
   constructor(
       private router: Router,
       private usuarioService: UsuarioService,
+      private cargoService: CargoService
       ) { }
 
-    public form: FormGroup;
-
-  constructor( private router: Router) { }
-
-
   ngOnInit(): void {
-      // this.criarUsuario();
+      this.criarFormulario();
   }
 
-  // public criarUsuario(): void{
-  //     this.form = this.formBilder.group({
-  //     });
-  // }
+  public criarFormulario(): void{
+      this.form = this.formBuilder.group({
+        id: [null],
+        nome: ['', Validators.required],
+        cpf: ['', Validators.required],
+        dataNascimento: ['', Validators.required],
+        email: ['', Validators.required],
+        status: [''],
+        telefone: [''],
+        cargo: [null, Validators.required]
+      });
+  }
 
   cancelar(): void {
       this.router.navigate(['/usuario']);
+  }
+
+  buscarCargos(): void{
+    this.cargoService.listar().subscribe((res: SelectItem[]) => {
+      this.cargos = [{
+        label: '==Escolha o Cargo==',
+        value: null
+      } as SelectItem].concat(res);
+    })
+  }
+
+  create(): void{
+    this.usuarioService.create(this.form.getRawValue());
   }
 
 }
